4d48838f892886992317133187e78b2633bdf194
mohamedmohamed60099187 <60099187@udst.edu.qa>
2025-10-23 14:50:06 +0300

    upload 4 images forest and mountain and city and beach and edit server .js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const businessLogic = require('./businessLogic');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Configure Handlebars
 app.engine('handlebars', exphbs.engine({
